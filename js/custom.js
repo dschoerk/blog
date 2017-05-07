7c20d22d16f9d208099b37b2c32906fa319f3553
rt <no@mail.com>
2017-05-07 10:51:07 +0200

    init

---
+++ js/custom.js
@@ -0,0 +1,58 @@
+// Closes the sidebar menu
+$("#menu-close").click(function(e) {
+  e.preventDefault();
+  $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Opens the sidebar menu
+$("#menu-toggle").click(function(e) {
+  e.preventDefault();
+  $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Scrolls to the selected menu item on the page
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+// Map scrolling behaviour
+$(document).ready(function() {
+  $('#map_iframe').addClass('scrolloff');
+  $('#map').on('click', function () {
+    $('#map_iframe').removeClass('scrolloff');
+  });
+
+  $('#map_iframe').mouseleave(function  () {
+    $('#map_iframe').addClass('scrolloff');
+  });
+});
+
+
+// navbar onscroll change class
+function checkScroll(){
+    var startY = $('.navbar').height(); //The point where the navbar changes in px
+
+    if($(window).scrollTop() > startY){
+        $('.navbar').addClass("scrolled");
+    }else{
+        $('.navbar').removeClass("scrolled");
+    }
+}
+
+if($('.navbar').length > 0){
+    $(window).on("scroll load resize", function(){
+        checkScroll();
+    });
+}
